350347084e8ac1b62bf9c1131ce7b1cba4b7d490
hbhatia <hbhatia@users.noreply.github.com>
2022-08-01 19:41:29 +0530

    Time: 1 ms (55.39%), Space: 40.7 MB (71.61%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,23 @@
+class Solution {
+    //Using Approach of DP 
+    //NO TLE in this approach
+    public int uniquePaths(int m, int n) {
+        int[][] matrix=new int[m][n];
+        // for(int[] arr:matrix){
+        //     Arrays.fill(matrix,1);
+        // }
+        //Or we can also fill only first and first column with value 1
+        for(int i=0;i<m;i++){
+            matrix[i][0]=1;
+        }
+        for(int i=0;i<n;i++){
+            matrix[0][i]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                matrix[i][j]=matrix[i][j-1]+matrix[i-1][j];
+            }
+        }
+        return matrix[m-1][n-1];
+    }
+}
