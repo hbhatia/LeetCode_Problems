c215968bc62284dee7eef5276ae2a31d7e3d7061
hbhatia <hbhatia@users.noreply.github.com>
2022-08-02 21:35:16 +0530

    Time: 1 ms (62.14%), Space: 43.9 MB (49.82%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -1,37 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    
-    //Running to know the running time only
-
-    private Deque<TreeNode> stack = new LinkedList();
-    private Deque<Integer> upperLimits = new LinkedList();
-    private Deque<Integer> lowerLimits = new LinkedList();
-
-    public void update(TreeNode root, Integer low, Integer high) {
-        stack.add(root);
-        lowerLimits.add(low);
-        upperLimits.add(high);
-    }
-
+    //Using the Solution apporach given in Solution
+    //Using Inorder recursive way
+    public Integer prev=null;
     public boolean isValidBST(TreeNode root) {
-        Integer low = null, high = null, val;
-        update(root, low, high);
-
-        while (!stack.isEmpty()) {
-            root = stack.poll();
-            low = lowerLimits.poll();
-            high = upperLimits.poll();
-
-            if (root == null) continue;
-            val = root.val;
-            if (low != null && val <= low) {
-                return false;
-            }
-            if (high != null && val >= high) {
-                return false;
-            }
-            update(root.right, val, high);
-            update(root.left, low, val);
+        if(null==root){
+            return true;
         }
-        return true;
+        if(!isValidBST(root.left)){
+            return false;
+        }
+        if(null!=prev && root.val<=prev){
+            return false;
+        }
+        prev=root.val;
+        return isValidBST(root.right);
     }
 }
