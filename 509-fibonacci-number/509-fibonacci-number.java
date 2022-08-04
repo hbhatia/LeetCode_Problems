bd87a7778ada5313515421243d8a7484a8e4e710
hbhatia <hbhatia@users.noreply.github.com>
2022-08-04 15:26:51 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (48.25%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,9 +1,13 @@
 class Solution {
-    //Using bruteforce recursion calls
+    //Using bruteforce Iteration calls
     public int fib(int n) {
-       if(n==0 || n==1){
-           return n;
-       } 
-       return fib(n-1) + fib(n-2);
+       int[] fib=new int[31];
+       fib[0]=0; 
+       fib[1]=1;
+       if(n<=1) return fib[n];
+       for(int i=2;i<=n;i++){
+           fib[i]=fib[i-1]+fib[i-2];
+       }
+        return fib[n];
     }
 }
