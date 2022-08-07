b62fcda59cef023da7b8035e698574f8705cd3dc
hbhatia <hbhatia@users.noreply.github.com>
2022-08-07 18:30:54 +0530

    Time: 15 ms (26.79%), Space: 42.4 MB (88.51%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.java
+++ 299-bulls-and-cows/299-bulls-and-cows.java
@@ -2,6 +2,7 @@ class Solution {
     //Using Array of size 10 i.e to keep values of 9 digits 
     //Using Approach given in this link
     //https://leetcode.com/problems/bulls-and-cows/discuss/74621/One-pass-Java-solution
+    //Slighly better concise ways for previous version
     public String getHint(String secret, String guess) {
         int count[]=new int[10];
         int bulls=0;
@@ -11,10 +12,10 @@ class Solution {
             char g=guess.charAt(i);
             if(s==g) bulls++;
             else{
-                if(count[s-'0']<0) cows++;
-                if(count[g-'0']>0) cows++;
-                count[s-'0']++;
-                count[g-'0']--;
+                if(count[s-'0']++<0) cows++;
+                if(count[g-'0']-->0) cows++;
+                // count[s-'0']++;
+                // count[g-'0']--;
             }
         }
         return new String(bulls+"A"+cows+"B");
