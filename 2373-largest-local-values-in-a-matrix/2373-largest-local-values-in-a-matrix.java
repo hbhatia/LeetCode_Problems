3ffcd8b609c057c4ae126e5380b0a5163b63ed73
hbhatia <hbhatia@users.noreply.github.com>
2022-08-14 12:28:43 +0530

    Time: 5 ms (25.00%), Space: 52.2 MB (25.00%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.java
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.java
@@ -1,17 +1,20 @@
 class Solution {
     //Uisng Solution in Discuss Section.
-    //https://leetcode.com/problems/largest-local-values-in-a-matrix/discuss/2422211/C%2B%2B-or-Simple-and-efficient-solution-or-TC%3AO((n-2)2)
+    //Using four loops -Brute force way
+    //Here we are targeting first possible and row and column accorindg to n-2 condition.
+    //And then we are just traversing to that partucular matrix and finding maximu value
+    //https://leetcode.com/problems/largest-local-values-in-a-matrix/discuss/2422378/Four-Loops
     public int[][] largestLocal(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
         int[][] res=new int[m-2][n-2];
-        for(int i=1;i<=m-2;i++){
-            for(int j=1;j<=n-2;j++){
-                int maxV=0;
-                maxV=Math.max(maxV,Math.max(grid[i-1][j-1],Math.max(grid[i-1][j],grid[i-1][j+1])));
-                maxV=Math.max(maxV,Math.max(grid[i][j-1],Math.max(grid[i][j],grid[i][j+1])));
-                maxV=Math.max(maxV,Math.max(grid[i+1][j-1],Math.max(grid[i+1][j],grid[i+1][j+1])));
-                res[i-1][j-1]=maxV;
+        for(int i=0;i<m-2;i++){
+            for(int j=0;j<n-2;j++){
+                for(int I1=i;I1<i+3;I1++){
+                    for(int I2=j;I2<j+3;I2++){
+                        res[i][j]=Math.max(res[i][j],grid[I1][I2]);
+                    }
+                }
             }
         }
         return res;
