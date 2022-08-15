8a29dc2bc94384c904b70b81036b56a8b0056e23
hbhatia <hbhatia@users.noreply.github.com>
2022-08-15 23:53:13 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (50.06%) - LeetHub

---
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    //Using BruteForce Approach to traverse tree recursively 
+    //Swap Left and right node of each root one by one
+    public TreeNode invertTree(TreeNode root) {
+        if(null!=root){
+            TreeNode left=root.left;
+            TreeNode right=root.right;
+            root.left=right;
+            root.right=left;
+            if(null!=root.left){
+                invertTree(root.left);
+            }
+            if(null!=root.right){
+                invertTree(root.right);
+            }
+        }
+        return root;
+    }
+}
