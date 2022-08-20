30898e3c4ac723cf98680674a9bbe5ea865ab3f3
hbhatia <hbhatia@users.noreply.github.com>
2022-08-20 22:56:06 +0530

    Time: 4 ms (5.29%), Space: 44.8 MB (7.14%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -1,44 +1,30 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    //Doing With BFS Approach like in approach 2
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(null==root){
-            return false;
-        }
-        LinkedList<TreeNode> stkN=new LinkedList<TreeNode>();
-        LinkedList<Integer> stkV=new LinkedList<Integer>();
-        stkN.push(root);
-        stkV.push(targetSum-root.val);
-        while(!stkN.isEmpty()){
-            TreeNode node=stkN.pop();
-            Integer val=stkV.pop();
-            if(null==node.left && null==node.right && val==0){
-                return true;
-            }
-            if(null!=node.right){
-                stkN.push(node.right);
-                stkV.push(val-node.right.val);
-            }
-            if(null!=node.left){
-                stkN.push(node.left);
-                stkV.push(val-node.left.val);
-            }
-        }
-        return false;
-        
+  public boolean hasPathSum(TreeNode root, int sum) {
+    if (root == null)
+      return false;
+
+    LinkedList<TreeNode> node_stack = new LinkedList();
+    LinkedList<Integer> sum_stack = new LinkedList();
+    node_stack.add(root);
+    sum_stack.add(sum - root.val);
+
+    TreeNode node;
+    int curr_sum;
+    while ( !node_stack.isEmpty() ) {
+      node = node_stack.pollLast();
+      curr_sum = sum_stack.pollLast();
+      if ((node.right == null) && (node.left == null) && (curr_sum == 0))
+        return true;
+
+      if (node.right != null) {
+        node_stack.add(node.right);
+        sum_stack.add(curr_sum - node.right.val);
+      }
+      if (node.left != null) {
+        node_stack.add(node.left);
+        sum_stack.add(curr_sum - node.left.val);
+      }
     }
+    return false;
+  }
 }
