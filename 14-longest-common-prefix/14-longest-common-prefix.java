102e5514b12fd0e8360eb4a65c265c15a97f769f
hbhatia <hbhatia@users.noreply.github.com>
2022-09-01 19:57:17 +0530

    Time: 4 ms (30.72%), Space: 42.2 MB (52.48%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,16 +1,32 @@
 class Solution {
-    //Based on Second Solution
-    //Vertical Scanning
+    //Based on Third Solution
+    //Divide and Conquer
     public String longestCommonPrefix(String[] strs) {
-        int len=strs.length;
-        for(int i=0;i<strs[0].length();i++){
-            char c=strs[0].charAt(i);
-            for(int j=1;j<len;j++){
-                if(i==strs[j].length() || c!=strs[j].charAt(i)){
-                    return strs[0].substring(0,i);
-                }
+        if(strs.length==1) return strs[0];
+        return findCommonPrefix(strs,0,strs.length-1);
+    }
+    
+    public String findCommonPrefix(String[] strs,int start,int end){
+        if(start==end){
+            return strs[start];
+        }
+        int mid=(start+end)/2;
+        String leftCommon=findCommonPrefix(strs,start,mid);
+        String rightCommon=findCommonPrefix(strs,mid+1,end);
+        return findCommonString(leftCommon,rightCommon);
+    }
+    
+    public String findCommonString(String left,String right){
+        StringBuilder commonStr=new StringBuilder();
+        int len=Math.min(left.length(),right.length());
+        for(int i=0;i<len;i++){
+            if(left.charAt(i)==right.charAt(i)){
+                commonStr.append(left.charAt(i));
+            }
+            else{
+                break;
             }
         }
-        return strs[0];
+        return commonStr.toString();
     }
 }
