faea43d15b570193a57813622837abe6161bff87
hbhatia <hbhatia@users.noreply.github.com>
2022-09-01 14:58:25 +0530

    Time: 1 ms (89.00%), Space: 42.1 MB (52.48%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,26 +1,15 @@
 class Solution {
-    //Using Solution 3 Provided by Leetcode
-    //Using Divide and Conquer approach
+    //Bases on First Solution
+    //Horizontal Scanning
     public String longestCommonPrefix(String[] strs) {
-        if(null==strs || strs.length==0) return "";
-        return findCommonPrefix(strs,0,strs.length-1);
-    }
-    public String findCommonPrefix(String strs[],int left ,int right){
-        if(left==right){
-            return strs[left];
-        }
-        int mid=(left+right)/2;
-        String leftCommon=findCommonPrefix(strs,left,mid);
-        String rightCommon=findCommonPrefix(strs,mid+1,right);
-        return findMatchedPrefix(leftCommon,rightCommon);
-    }
-    public String findMatchedPrefix(String left,String right){
-        int minLen=Math.min(left.length(),right.length());
-        for(int i=0;i<minLen;i++){
-            if(left.charAt(i)!=right.charAt(i)){
-                return left.substring(0,i);
+        if(strs.length==1) return strs[0];
+        String prefix=strs[0];
+        for(int i=1;i<strs.length;i++){
+            while(strs[i].indexOf(prefix)!=0){
+                prefix=prefix.substring(0,prefix.length()-1);
             }
+            if(prefix.isEmpty()) return "";
         }
-        return left.substring(0,minLen);
+        return prefix;
     }
 }
