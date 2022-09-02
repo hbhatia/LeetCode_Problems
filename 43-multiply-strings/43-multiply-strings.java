962d277d13e3a76f2355e3b47d0b345b9b73e56c
hbhatia <hbhatia@users.noreply.github.com>
2022-09-02 10:48:58 +0530

    Time: 6 ms (81.14%), Space: 42.9 MB (68.10%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.java
+++ 43-multiply-strings/43-multiply-strings.java
@@ -1,33 +1,30 @@
 class Solution {
-    //Using Solution given in discuss section
-    //Link - https://leetcode.com/problems/multiply-strings/discuss/17608/AC-solution-in-Java-with-explanation
-    //Here we are computing digits by digits product and then storing these product in one array of size M+N
+    //Doing by the Solution Given in discuss Section
+    //Link -https://leetcode.com/problems/multiply-strings/discuss/17605/Easiest-JAVA-Solution-with-Graph-Explanation
     public String multiply(String num1, String num2) {
-        int M=num1.length();   //num1 length
-        int N=num2.length();   //num2.length
-        
+        int M=num1.length();
+        int N=num2.length();
         int res[]=new int[M+N];
         
-        //Strat computing
+        //Start Calculate the multiplication
         for(int i=M-1;i>=0;i--){
             for(int j=N-1;j>=0;j--){
+                int p1=i+j,p2=p1+1;
                 int d1=num1.charAt(i)-'0';
                 int d2=num2.charAt(j)-'0';
-                res[i+j+1]+=d1*d2;
+                int mult=res[p2]+(d1*d2);
+                res[p1]+=mult/10;
+                res[p2]=mult%10;
             }
         }
-        int carry=0;
-        for(int i=M+N-1;i>=0;i--){
-            int sum=carry+res[i];
-            carry=sum/10;
-            res[i]=sum%10;
+        int i=0;
+        StringBuilder multString=new StringBuilder();
+        while(i<(M+N)){
+            if(!(res[i]==0 && multString.isEmpty())){
+                multString.append(res[i]);
+            }
+            i++;
         }
-        StringBuilder sb=new StringBuilder("");
-        for(int num:res){
-            sb.append(num);
-        }
-        while(sb.length()!=0 && sb.charAt(0)=='0') sb.deleteCharAt(0);
-        
-        return sb.length()==0?"0":sb.toString();
+        return multString.isEmpty()?"0":multString.toString();
     }
 }
