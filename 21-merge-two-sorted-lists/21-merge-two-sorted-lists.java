7a11de2fd6f5390de84919a918ba8574f3011460
hbhatia <hbhatia@users.noreply.github.com>
2022-10-05 16:33:26 +0530

    Time: 1 ms (82.02%), Space: 42.6 MB (79.13%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -17,13 +17,12 @@ class Solution {
             if(list1.val<=list2.val){
                 l3.next=list1;
                 list1=list1.next;
-                l3=l3.next;
             }
             else{
                 l3.next=list2;
                 list2=list2.next;
-                l3=l3.next;
             }
+            l3=l3.next;
         }
         l3.next=(list1==null?list2:list1);
         return ptr.next;
