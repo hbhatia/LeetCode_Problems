80cab52d875f436efce646ab9be8baeca8a300b0
hbhatia <hbhatia@users.noreply.github.com>
2022-10-25 22:16:20 +0530

    Time: 3 ms (60.79%), Space: 42.2 MB (10.62%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,25 +1,20 @@
 class Solution {
-    
+    //Using the MaxHeap Solution 
+    //Using Priority Queue approach
     public int lastStoneWeight(int[] stones) {
-        
-        // Insert all the stones into a Max-Heap.
-        PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
-        for (int stone: stones) {
+        PriorityQueue<Integer> heap=new PriorityQueue<Integer>(Comparator.reverseOrder());
+        for(int stone:stones){
             heap.add(stone);
         }
-
-        // While there is more than one stone left, we need to remove the two largest
-        // and smash them together. If there is a resulting stone, we need to put into
-        // the heap.
-        while (heap.size() > 1) {
-            int stone1 = heap.remove();
-            int stone2 = heap.remove();
-            if (stone1 != stone2) {
-                heap.add(stone1 - stone2);
+        //Treverse the heap and minimise the elements as per the problem.
+        while(heap.size()>1){
+            int stone1=heap.remove();
+            int stone2=heap.remove();
+            if(stone1!=stone2){
+                heap.add(stone1-stone2);
             }
         }
-
-        // Check whether or not there is a stone left to return.
-        return heap.isEmpty() ? 0 : heap.remove();
+        //Finaly return the element.
+        return heap.size()==0?0:heap.remove();
     }
 }
