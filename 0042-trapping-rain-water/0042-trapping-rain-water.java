f4dc33864a21affb1eefbee20ff743bd10d9722e
hbhatia <hbhatia@users.noreply.github.com>
2022-10-26 23:00:18 +0530

    Time: 2 ms (66.40%), Space: 49.4 MB (15.66%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,23 @@
+class Solution {
+    //Using Brute Force Approach
+    public int trap(int[] height) {
+        int len=height.length;
+        int leftMax[]=new int[len];
+        int rightMax[]=new int[len];
+        leftMax[0]=height[0];
+        rightMax[len-1]=height[len-1];
+        
+        for(int i=1;i<len;i++){
+            leftMax[i]=Math.max(leftMax[i-1],height[i]);
+        }
+        
+        for(int i=len-2;i>=0;i--){
+            rightMax[i]=Math.max(rightMax[i+1],height[i]);
+        }
+        int countWater=0;
+        for(int i=0;i<len;i++){
+            countWater+=Math.min(leftMax[i],rightMax[i])-height[i];
+        }
+        return countWater;
+    }
+}
