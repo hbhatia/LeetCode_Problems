a84a6df27e081a4a465d8b7c5d901cef89c9ff26
hbhatia <hbhatia@users.noreply.github.com>
2022-10-29 17:43:27 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (87.27%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,23 +1,29 @@
 class Solution {
-    //Using Brute Force Approach
-    //This has TC- O(N) and SC is O(N)
+    //Using 4th Approch in Solution of Two Pointer Approach
     public int trap(int[] height) {
-        int len=height.length;
-        int leftMax[]=new int[len];
-        int rightMax[]=new int[len];
-        leftMax[0]=height[0];
-        rightMax[len-1]=height[len-1];
-        
-        for(int i=1;i<len;i++){
-            leftMax[i]=Math.max(leftMax[i-1],height[i]);
-        }
-        
-        for(int i=len-2;i>=0;i--){
-            rightMax[i]=Math.max(rightMax[i+1],height[i]);
-        }
+        int left=0;
+        int right=height.length-1;
         int countWater=0;
-        for(int i=0;i<len;i++){
-            countWater+=Math.min(leftMax[i],rightMax[i])-height[i];
+        int leftMax=0,rightMax=0;
+        while(left<right){
+            if(height[left]<height[right]){
+                if(height[left]>=leftMax){
+                    leftMax=height[left];
+                }
+                else{
+                    countWater+=leftMax-height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right]>=rightMax){
+                    rightMax=height[right];
+                }
+                else{
+                    countWater+=rightMax-height[right];
+                }
+                right--;
+            }
         }
         return countWater;
     }
