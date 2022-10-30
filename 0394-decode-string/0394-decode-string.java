2c8614115792c59b9d97cc9052b9130f21467cc9
hbhatia <hbhatia@users.noreply.github.com>
2022-10-30 21:53:00 +0530

    Time: 1 ms (94.07%), Space: 40.2 MB (93.43%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,40 +1,32 @@
 class Solution {
-    //Using first Approach given in Solution
+    //Using 2nd Approach in the Solution
+    //Here we will use 2 Stack to keep Count and String in Seprate Stack.
     public String decodeString(String s) {
-        Stack<Character> stk=new Stack<Character>();
-        for(int i=0;i<s.length();i++){
-            Character ch=s.charAt(i);
-            if(ch==']'){
-                //Need to pop the values from Stack
-                StringBuilder str=new StringBuilder();
-                while(!stk.isEmpty() && stk.peek()!='['){
-                    str.append(stk.pop());
+        Stack<Integer> cStack=new Stack<Integer>();
+        Stack<StringBuilder> sStack=new Stack<StringBuilder>();
+        StringBuilder currStr=new StringBuilder();
+        int k=0;
+        for(char ch : s.toCharArray()){
+            if(Character.isDigit(ch)){
+                k=k*10+(ch-'0');
+            }
+            else if(ch=='['){
+                cStack.push(k);
+                k=0;
+                sStack.push(currStr);
+                currStr=new StringBuilder();
+            }
+            else if(ch==']'){
+                StringBuilder prevString=sStack.pop();
+                for(int countNum=cStack.pop();countNum>0;countNum--){
+                    prevString.append(currStr);
                 }
-                //Delete the [ from stack
-                stk.pop();
-                //Findinh the number to whihc we will multiple the occurance
-                int num=0;
-                int base=1;
-                while(!stk.isEmpty() && Character.isDigit(stk.peek())){
-                    num=num+(stk.pop()-'0')*base;
-                    base=base*10;
-                }
-                //We will reverse push the string num times in the stack again
-                while(num>0){
-                    for(int k=str.length()-1;k>=0;k--){
-                        stk.push(str.charAt(k));
-                    }
-                    num--;
-                }
+                currStr=prevString;
             }
             else{
-                stk.push(ch);
+                currStr.append(ch);
             }
         }
-        StringBuilder output=new StringBuilder();
-        while(!stk.isEmpty()){
-            output.append(stk.pop());
-        }
-        return output.reverse().toString();
+        return currStr.toString();
     }
 }
