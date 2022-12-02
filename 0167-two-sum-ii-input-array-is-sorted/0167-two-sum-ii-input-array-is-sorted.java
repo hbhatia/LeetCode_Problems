188545bb81e27dacf28677e026e4d0d3323174a1
hbhatia <hbhatia@users.noreply.github.com>
2022-12-03 00:27:47 +0530

    Time: 10 ms (32.94%), Space: 50.2 MB (39.61%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,18 +1,21 @@
 class Solution {
-    //Using the brute force Approcah
-    //TC will be problem in this code as TC is O(N^2)
-    //It is giving TLE.
+    //Using Two Pointer Apporach
     public int[] twoSum(int[] numbers, int target) {
         int len=numbers.length;
-        int[] retArr=new int[2];
-        for(int i=0;i<len;i++){
-            int val1=numbers[i];
-            for(int j=i+1;j<len && val1+numbers[j]<=target;j++){
-                if(val1+numbers[j]==target){
-                    return new int[]{i+1,j+1};
-                }
+        int low=0;
+        int high=len-1;
+        while(low<high){
+            int sum=numbers[low]+numbers[high];
+            if(sum==target){
+                return new int[]{low+1,high+1};
+            }
+            else if(sum<target){
+                low++;
+            }
+            else{
+                high--;
             }
         }
-        return retArr;
+        return new int[2];
     }
 }
